8810edb52dd53526ca2d4a464ea3576796f353fa
kRenelt <43582922+kRenelt@users.noreply.github.com>
2020-05-28 16:16:26 -0500

    Delete db.ts
    delete this

--- src/daos/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-
-export const db = new Pool({
-    database: 'postgres',
-    host: process.env.NODE_APP_URL,
-    port: 5432,
-    user: process.env.NODE_APP_P0_ROLE,
-    password: process.env.P0_KR_PASS
-});
