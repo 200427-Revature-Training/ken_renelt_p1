c248e31573a5943549e4ce79502e3b69b52617a4
kRenelt <43582922+kRenelt@users.noreply.github.com>
2020-05-28 16:20:19 -0500

    adding daos back

---
+++ ken_renelt_p1/src/routers/user-router.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import * as userService from '../services/user-service';
+
+export const userRouter = express.Router();
+
+const users = [];
+
+userRouter.get('', (request, response, next) => {
+    console.log('i am user-router');
+   userService.getAllUsers().then(users => {
+    response.json(users);
+    next();
+   }).catch(err => {
+       console.log(err);
+       response.sendStatus(500);
+   });
+});
